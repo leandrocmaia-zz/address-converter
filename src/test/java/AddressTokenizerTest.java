f0b932b4feeda38ab10fc7be8adf0e45f1f1c124
leandro <leandro@192.168.0.2>
2017-05-11 17:58:53 +0200

    removed spring runner from unit test

--- src/test/java/AddressTokenizerTest.java
+++ src/test/java/AddressTokenizerTest.java
@@ -2,15 +2,10 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import de.friday.domain.AddressToken;
 import de.friday.util.AddressTokenizer;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {AddressTokenizerTest.class})
 public class AddressTokenizerTest {
 
 	private AddressTokenizer addressTokenizer;
